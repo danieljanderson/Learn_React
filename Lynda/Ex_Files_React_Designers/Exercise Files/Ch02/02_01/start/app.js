483451db45ac90c1e4646ec7d87d034130920f01
danieljanderson <danieljanderson0616@gmail.com>
2019-04-01 10:48:40 -0400

    Completed 2.1

--- Lynda/Ex_Files_React_Designers/Exercise Files/Ch02/02_01/start/app.js
+++ Lynda/Ex_Files_React_Designers/Exercise Files/Ch02/02_01/start/app.js
@@ -2,5 +2,10 @@
   "use strict";
 
   // Start here
-
+  var ProductCustomizer = React.createElement(
+    "div",
+    { className: "customizer" },
+    "Product customizer will go here"
+  );
+  ReactDom.render(ProductCustomizer, document.getElementById("react-root"));
 })();
