877e50a630003e067617ee6023167fc1f7309eae
danieljanderson <danieljanderson0616@gmail.com>
2019-04-01 11:20:25 -0400

    completed 2.2

--- Lynda/Ex_Files_React_Designers/Exercise Files/Ch02/wip/start/app.js
+++ Lynda/Ex_Files_React_Designers/Exercise Files/Ch02/wip/start/app.js
@@ -1,11 +1,28 @@
 (function() {
   "use strict";
+  // the create react element is first the type of element.  Then the properties of said element
+  //Always Capitalize the name of your components. React needs to know the difference  between the actual components and regular html elements like divs.
+  function ProductImage(props) {
+    return React.createElement("img", {
+      src: "../../../assets/red.jpg",
+      alt: "Product image"
+    });
+  }
 
-  // Start here
-  var ProductCustomizer = React.createElement(
-    "div",
-    { className: "customizer" },
-    "Product customizer will go here"
+  function ProductCustomizer(props) {
+    return React.createElement(
+      "div",
+      { className: "customizer" },
+      React.createElement(
+        "div",
+        { className: "product-image" },
+        React.createElement(ProductImage)
+      )
+    );
+  }
+
+  ReactDOM.render(
+    React.createElement(ProductCustomizer),
+    document.getElementById("react-root")
   );
-  ReactDOM.render(ProductCustomizer, document.getElementById("react-root"));
 })();
