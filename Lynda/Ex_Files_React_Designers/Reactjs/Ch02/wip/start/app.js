3be43ee2db8b0378af0eb23281cc5c1ef79ab160
danieljanderson <danieljanderson0616@gmail.com>
2019-04-03 17:20:18 -0400

    Not green

--- Lynda/Ex_Files_React_Designers/Reactjs/Ch02/wip/start/app.js
+++ Lynda/Ex_Files_React_Designers/Reactjs/Ch02/wip/start/app.js
@@ -2,6 +2,26 @@
   "use strict";
   // the create react element is first the type of element.  Then the properties of said element
   //Always Capitalize the name of your components. React needs to know the difference  between the actual components and regular html elements like divs.
+  function SizeSelector(props) {
+    function sizeOptions() {
+      var size = window.Inventory.allSizes;
+      return sizes.map(function(num) {
+        return (
+          <option value={num} key={num}>
+            Size:{num}
+          </option>
+        );
+      });
+    }
+    return (
+      <div className="field-group">
+        <label htmlFor="size-options">Size:</label>
+        <select name="sizeOptions" id="size-options">
+          {sizeOptions}
+        </select>/>
+      </div>
+    );
+  }
   function ProductImage(props) {
     return <img src="../../../assets/red.jpg" alt="Product image" />;
   }
@@ -11,6 +31,9 @@
       <div className="customizer">
         <div className="product-image">
           <ProductImage />
+        </div>
+        <div className="selectors">
+          <SizeSelector />
         </div>
       </div>
     );
