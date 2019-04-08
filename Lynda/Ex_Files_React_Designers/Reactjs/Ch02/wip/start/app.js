5ba8e0b7137a352bd9994d561555069d17224b1d
danieljanderson <danieljanderson0616@gmail.com>
2019-04-08 09:48:12 -0400

    done with 2.7

--- Lynda/Ex_Files_React_Designers/Reactjs/Ch02/wip/start/app.js
+++ Lynda/Ex_Files_React_Designers/Reactjs/Ch02/wip/start/app.js
@@ -1,39 +1,43 @@
+//http://localhost/Reactjs/Ch02/wip/start/index.html
 (function() {
   "use strict";
-  // the create react element is first the type of element.  Then the properties of said element
-  //Always Capitalize the name of your components. React needs to know the difference  between the actual components and regular html elements like divs.
+
   function SizeSelector(props) {
     function sizeOptions() {
-      var size = window.Inventory.allSizes;
+      var sizes = window.Inventory.allSizes;
+
       return sizes.map(function(num) {
         return (
           <option value={num} key={num}>
-            Size:{num}
+            Size:{num}>
+            {num}
           </option>
         );
       });
     }
+
     return (
       <div className="field-group">
         <label htmlFor="size-options">Size:</label>
-        <select name="sizeOptions" id="size-options">
-          {sizeOptions}
-        </select>/>
+        <select defaultValue={props.size} name="sizeOptions" id="size-options">
+          {sizeOptions()}
+        </select>
       </div>
     );
   }
+
   function ProductImage(props) {
-    return <img src="../../../assets/red.jpg" alt="Product image" />;
+    return <img src={`../../../assets/${props.color}.jpg`} alt="Product Image" />;
   }
 
   function ProductCustomizer(props) {
     return (
       <div className="customizer">
         <div className="product-image">
-          <ProductImage />
+          <ProductImage color="red" />
         </div>
         <div className="selectors">
-          <SizeSelector />
+          <SizeSelector size={8} />
         </div>
       </div>
     );
